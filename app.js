4e6b2099dff0f996da157096060625484c5c2271
Jakub Holub <60482905+HenyPotter@users.noreply.github.com>
2024-04-23 17:12:19 +0200

    feat: add index page and console logging for all listening routes

--- app.js
+++ app.js
@@ -27,12 +27,16 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+app.get('/live', (req, res) => {
+  res.render('live');
+});
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/data', (req, res) => {
-  connection.query('SELECT time, temperature, pressure, humidity, latitude, longitude, height, velocity FROM sarfad_data', (error, results, fields) => {
+  connection.query('SELECT DATE_FORMAT(timestamp, "%H:%i") AS time, temperature, pressure, humidity, latitude, longitude, height, velocity FROM sarfad_data', (error, results, fields) => {
     if (error) {
       console.error('Error executing query:', error);
       res.status(500).json({ error: 'Internal server error' });
@@ -43,7 +47,17 @@ app.get('/data', (req, res) => {
   });
 });
 
+const colors = {
+  green: '\x1b[32m',
+  white: '\x1b[37m',
+  reset: '\x1b[0m'
+};
+
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`${colors.green}App listening on the following routes:${colors.reset}`);
+  console.log(`${colors.white}---------------------------------------${colors.reset}`);
+  console.log(`${colors.green}Index Page: ${colors.reset}http://localhost:${port}`);
+  console.log(`${colors.green}Live Web: ${colors.reset}http://localhost:${port}/live`);
+  console.log(`${colors.white}---------------------------------------${colors.reset}`);
 });
   
