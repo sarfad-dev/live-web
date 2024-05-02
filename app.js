5c3907846bdb182709ffa859624c2d8d023f6014
Jakub Holub <60482905+HenyPotter@users.noreply.github.com>
2024-05-02 14:02:55 +0200

    feat: control app for an easy troubleshooting of an mission

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.get('/live', (req, res) => {
   res.render('live');
 });
 
+app.get('/control', (req, res) => {
+  res.render('control');
+});
+
 app.get('/locate', (req, res) => {
   res.render('locate');
 });
@@ -63,7 +67,6 @@ app.get('/live-data', (req, res) => {
   );
 });
 
-
 app.get('/latest-location', (req, res) => {
   const query = 'SELECT latitude, longitude, MAX(timestamp) AS latest_timestamp, temperature, pressure, humidity, height, velocity FROM sarfad_data GROUP BY latitude, longitude ORDER BY latest_timestamp DESC LIMIT 1;';
   connection.query(query, (error, results, fields) => {
@@ -88,6 +91,41 @@ app.get('/check-password', (req, res) => {
   }
 });
 
+app.get('/save-launch-time', (req, res) => {
+  const launchTimeISO = req.query.launchTime;
+  if (launchTimeISO) {
+      // Convert the ISO date to MySQL date format
+      const launchTime = new Date(launchTimeISO);
+      const formattedLaunchTime = `${launchTime.getFullYear()}-${String(launchTime.getMonth() + 1).padStart(2, '0')}-${String(launchTime.getDate()).padStart(2, '0')} ${String(launchTime.getHours()).padStart(2, '0')}:${String(launchTime.getMinutes()).padStart(2, '0')}:${String(launchTime.getSeconds()).padStart(2, '0')}`;
+
+      const query = 'INSERT INTO launch_time (launch_time) VALUES (?) ON DUPLICATE KEY UPDATE launch_time = VALUES(launch_time)';
+      connection.query(query, [formattedLaunchTime], (error) => {
+          if (error) {
+              console.error('Error saving launch time:', error);
+              res.status(500).json({ error: 'Internal server error' });
+              return;
+          }
+          res.status(200).end();
+      });
+  } else {
+      res.status(400).json({ error: 'Missing launch time parameter' });
+  }
+});
+
+
+app.get('/get-launch-time', (req, res) => {
+  const query = 'SELECT launch_time FROM launch_time ORDER BY id DESC LIMIT 1';
+  connection.query(query, (error, results) => {
+      if (error) {
+          console.error('Error fetching launch time:', error);
+          res.status(500).json({ error: 'Internal server error' });
+          return;
+      }
+      const launchTime = results[0] ? results[0].launch_time : null;
+      res.json({ launchTime });
+  });
+});
+
 const colors = {
   green: '\x1b[32m',
   white: '\x1b[37m',
@@ -100,5 +138,6 @@ app.listen(port, () => {
   console.log(`${colors.green}Index Page: ${colors.reset}http://localhost:${port}/`);
   console.log(`${colors.green}Live Web: ${colors.reset}http://localhost:${port}/live`);
   console.log(`${colors.green}Locate App: ${colors.reset}http://localhost:${port}/locate`);
+  console.log(`${colors.green}Control center: ${colors.reset}http://localhost:${port}/control`);
   console.log(`${colors.white}---------------------------------------${colors.reset}`);
 });
